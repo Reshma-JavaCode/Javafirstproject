f6e108a070cea36bc6bc9df4b27c0e83856c0eb1
Reshma-Javacode <Reshma.mohd2508@gmail.com>
2026-06-24 17:29:08 +0530

    bubble sort,selection sort

---
+++ src/com/arrays/BubbleSort.java
@@ -0,0 +1,66 @@
+package com.arrays;
+
+//Bubble Sort
+//comparing with all its adjusent elements from array 
+//and swap them if they r in wrong order
+//sorting happens from end like:
+//big ele kept in last position
+//4,3,2,1 ---4 is big so,it comes first to its position i.e last 3,2,1,4 etc
+public class BubbleSort {
+
+	static void bubbleSort(int[] a)
+	{
+		//to know iterations take count		
+		int count1=0, count2=0;
+		
+		for(int i=0;i<a.length-1;i++)
+		{ 
+			boolean status=false;
+			count1++;
+			//so,here loop gng by skipping last element position
+			//since,last element places 1st to its positioni.e 4 at 3rd index
+			//now,no need to compare 3rd index etc
+			for(int j=0;j<a.length-1-i;j++)
+			{count2++;
+				if(a[j]>a[j+1])
+				{
+					int temp= a[j];
+					a[j]= a[j+1];
+					a[j+1]= temp; 
+					status=true;
+				}
+			}
+			
+			if(!status)
+			{
+				break;
+			}
+		}
+		
+		System.out.println(count1);
+		System.out.println(count2);
+	}
+	public static void main(String[] args) {
+		
+		//iteration count i loop= 1time,j loop=3times
+		//because of break;TC= O(n); otherwise TC= O(n-square)
+		//int a[]= {1,2,3,4}; 
+		int a[]= {4,3,1,2};
+		
+		System.out.println("Before Sorting...");
+		for(int i:a)
+		{
+			System.out.print(i+" ");		
+		}
+		
+		System.out.println("\nafter Bubble sort...");
+		bubbleSort(a);
+		for(int i:a)
+		{
+			System.out.print(i+" ");		
+		}
+		
+
+	}
+
+}
---
+++ src/com/arrays/SelectionSort.java
@@ -0,0 +1,49 @@
+package com.arrays;
+
+public class SelectionSort {
+//Selection sort
+	//finds smallest ele fron unsorted part of array
+	//and place it as its correct position
+	//smallest ele places 1st to its position i.e,
+	//4,3,1,2 -> 1 comes 1st to 0th position 1,3,4,2 etc
+	static void selectionSort(int[] a)
+	{
+		
+		for(int i=0;i<a.length-1;i++)
+		{
+			int minIndex=i;
+			//so, here gng loop till end of array sz
+			//since,1 at 0th index 
+			//placed at its actual position acc. to sorting
+			//so,no need to compare that 0th position everytime
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[j]< a[minIndex])
+				{
+					minIndex = j;
+				}
+			}
+			
+			//swapping positions values
+			int temp= a[i];
+			a[i]= a[minIndex];
+			a[minIndex]= temp;
+		}
+	}
+	public static void main(String[] args) {
+
+		int a[] = { 4, 3, 1, 2 };
+
+		System.out.println("Before Sorting...");
+		for (int i : a) {
+			System.out.print(i + " ");
+		}
+
+		System.out.println("\nafter selection sort...");
+		selectionSort(a);
+		for (int i : a) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
